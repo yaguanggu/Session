c407619458f425147087562e265a4526d0b157d1
Kiran Challa <kichalla@microsoft.com>
2015-05-28 17:22:33 -0700

    Initial

---
+++ src/Microsoft.AspNet.Session.Providers.SqlServer/Session.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNet.Session.Providers.SqlServer
+{
+    public class Session
+    {
+        //todo:remove later: id from the session cookie
+        public string Id { get; set; }
+
+        //todo:remove later: serialized session data having both keys and their values.
+        public byte[] Value { get; set; }
+
+        public DateTimeOffset LastAccessedInUTC { get; set; }
+    }
+}
---
+++ src/Microsoft.AspNet.Session.Providers.SqlServer/SqlServerCache.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Framework.Caching.Distributed;
+
+namespace Microsoft.AspNet.Session.Providers.SqlServer
+{
+    public class SqlServerCache : IDistributedCache
+    {
+        public void Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ConnectAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Get(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<byte[]> GetAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Refresh(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RefreshAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(string key, byte[] value, DistributedCacheEntryOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetAsync(string key, byte[] value, DistributedCacheEntryOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
